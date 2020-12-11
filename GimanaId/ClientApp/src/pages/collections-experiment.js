0b97fb7b81522cdace220cd5f395b163150d7e24
Farrel R <rafirizqy12@gmail.com>
2020-12-11 23:55:31 +0700

    Enable adding steps in the add-article prototype

--- GimanaId/ClientApp/src/pages/collections-experiment.js
+++ GimanaId/ClientApp/src/pages/collections-experiment.js
@@ -1,48 +1,53 @@
 import { useState } from "react";
 
 const TestArticleCollectionsPage = () => {
-    const blankValues = {
-        title: "",
-        description: "",
-        parts: []
+    const blanks = {
+        meta: {
+            title: "",
+            description: ""
+        }
     };
 
-    const [values, setValues] = useState(blankValues);
+    blanks.step = {
+        // stepNumber: null,
+        title: "",
+        description: ""
+    };
 
-    // const []
+    blanks.part = {
+        // partNumber: null,
+        title: "",
+        description: "",
+        steps: [blanks.step]
+    };
+
+    const [mainArticleData, setMainArticleData] = useState(blanks.meta);
+    const [parts, setParts] = useState([blanks.part]);
+
 
     // Set one part of the whole state object/value
-    function setOneProperty(property) {
-        setValues(currentValues => ({ ...currentValues, ...property }));
-    }
-
-    function onTitleChange(ev) {
-        setOneProperty({ title: ev.target.value });
-    }
-
-    function onDescriptionChange(ev) {
-        setOneProperty({ description: ev.target.value });
+    function partiallyUpdateStateObject(stateSetterFunction, newProperty) {
+        stateSetterFunction(unchangedValues => ({
+            ...unchangedValues,
+            ...newProperty
+        }));
     }
 
     function addPart() {
-        const currentParts = values.parts;
-        currentParts.push({
-            title: "",
-            description: "",
-            steps: [
-                {
-                    title: "Title",
-                    description: "Deskription"
-                }
-            ]
-        });
-        setOneProperty({ parts: currentParts })
+        const currentParts = [...parts];
+        currentParts.push(blanks.part);
+        setParts(currentParts);
+    }
+
+    function addStep(partIndexNumber) {
+        const currentParts = [...parts];
+        currentParts[partIndexNumber].steps.push(blanks.step);
+        setParts(currentParts);
     }
 
     async function handleSubmit(ev) {
         ev.preventDefault();
         console.log("Submitting...");
-        console.log(JSON.stringify(values));
 
         const submissionResponse = await fetch(`api/articles`, {
             method: "POST",
@@ -50,12 +55,16 @@ const TestArticleCollectionsPage = () => {
                 accept: "application/json",
                 "Content-Type": "application/json"
             },
-            body: JSON.stringify(values)
+            body: JSON.stringify({
+                ...mainArticleData,
+                parts: parts
+            })
         });
 
         if (submissionResponse.ok) {
             console.log("Article submitted successfully!");
-            setValues(blankValues);
+            setMainArticleData(blanks.meta);
+            setParts([blanks.part]);
         }
         else {
             console.log(submissionResponse);
@@ -67,29 +76,56 @@ const TestArticleCollectionsPage = () => {
             <div className="articles-collection-container">
                 <form onSubmit={handleSubmit}>
                     <label>Judul
-                        <input type="text" required value={values.title} onChange={onTitleChange} />
+                        <input type="text" required value={mainArticleData.title} onChange={ev => {
+                            partiallyUpdateStateObject(setMainArticleData, { title: ev.target.value });
+                        }} />
                     </label>
                     <label>Deskripsi
-                        <textarea value={values.description} onChange={onDescriptionChange} />
+                        <textarea value={mainArticleData.description} onChange={ev => {
+                            partiallyUpdateStateObject(setMainArticleData, { description: ev.target.value });
+                        }} />
                     </label>
                     <ul>
-                        {values.parts.map((entry, indexNumber) => (
+                        {parts.map((part, partIndex) => (
+                            // FIXME on the `key` prop (I don't know if using the index number alone is suitable)
                             <li style={{ display: `block`, border: `2px solid black`, padding: `1em` }}>
-                                <h1>Part {indexNumber + 1}</h1>
+                                <h1>Part {partIndex + 1}</h1>
                                 <label>Judul part:
-                                <input type="text" required value={entry.title} onChange={ev => {
-                                        const val = [...values.parts];
-                                        val[indexNumber].title = ev.target.value;
-                                        setOneProperty({ parts: val });
+                                    <input type="text" required value={part.title} onChange={ev => {
+                                        const val = [...parts];
+                                        val[partIndex].title = ev.target.value;
+                                        setParts(val);
                                     }} />
                                 </label>
                                 <label>Deskripsi:
-                                <textarea value={entry.description} onChange={ev => {
-                                        const val = [...values.parts];
-                                        val[indexNumber].description = ev.target.value;
-                                        setOneProperty({ parts: val });
+                                    <textarea value={part.description} onChange={ev => {
+                                        const val = [...parts];
+                                        val[partIndex].description = ev.target.value;
+                                        setParts(val);
                                     }} />
                                 </label>
+                                <ul>
+                                    {part.steps.map((step, stepIndex) => (
+                                        <li>
+                                            <h2>Step {stepIndex + 1}</h2>
+                                            <label>Judul step:
+                                                <input type="text" required value={step.title} onChange={ev => {
+                                                    const partsVal = [...parts];
+                                                    partsVal[partIndex].steps[stepIndex].title = ev.target.value;
+                                                    setParts(partsVal);
+                                                }} />
+                                            </label>
+                                            <label>Deskripsi step:
+                                                <textarea value={step.description} onChange={ev => {
+                                                    const partsVal = [...parts];
+                                                    partsVal[partIndex].steps[stepIndex].description = ev.target.value;
+                                                    setParts(partsVal);
+                                                }} />
+                                            </label>
+                                        </li>
+                                    ))}
+                                </ul>
+                                <button type="button" onClick={() => { addStep(partIndex) }}>Add step</button>
                             </li>
                         ))}
                     </ul>
