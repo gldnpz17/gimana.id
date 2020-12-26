7e3bcf41caf3c305b05ae1ae5aa372d643e415f5
firdausbismasuryakusuma <firdausbismasuryakusuma@365.ugm.ac.id>
2020-12-26 23:11:11 +0700

    added database migration and modified docker file. also migrated to .NET Core 5.0

---
+++ Infrastructure/PostgresDatabase/Migrations/20201226160454_InitMigration.cs
@@ -0,0 +1,295 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace PostgresDatabase.Migrations
+{
+    public partial class InitMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Image",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false, defaultValue: new Guid("9cff84af-9a3a-433a-b4f0-23ca8dff2631")),
+                    FileFormat = table.Column<string>(type: "text", nullable: true),
+                    Base64EncodedData = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Image", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Articles",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Title = table.Column<string>(type: "text", nullable: true),
+                    Description = table.Column<string>(type: "text", nullable: true),
+                    HeroImageId = table.Column<Guid>(type: "uuid", nullable: true),
+                    DateCreated = table.Column<DateTime>(type: "timestamp without time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Articles", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Articles_Image_HeroImageId",
+                        column: x => x.HeroImageId,
+                        principalTable: "Image",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Username = table.Column<string>(type: "text", nullable: true),
+                    ProfilePictureId = table.Column<Guid>(type: "uuid", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Users_Image_ProfilePictureId",
+                        column: x => x.ProfilePictureId,
+                        principalTable: "Image",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ArticlePart",
+                columns: table => new
+                {
+                    ArticleId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    PartNumber = table.Column<int>(type: "integer", nullable: false),
+                    Title = table.Column<string>(type: "text", nullable: true),
+                    Description = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ArticlePart", x => new { x.ArticleId, x.Id });
+                    table.ForeignKey(
+                        name: "FK_ArticlePart_Articles_ArticleId",
+                        column: x => x.ArticleId,
+                        principalTable: "Articles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ArticleUser",
+                columns: table => new
+                {
+                    ArticlesId = table.Column<Guid>(type: "uuid", nullable: false),
+                    UsersId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ArticleUser", x => new { x.ArticlesId, x.UsersId });
+                    table.ForeignKey(
+                        name: "FK_ArticleUser_Articles_ArticlesId",
+                        column: x => x.ArticlesId,
+                        principalTable: "Articles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ArticleUser_Users_UsersId",
+                        column: x => x.UsersId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AuthTokens",
+                columns: table => new
+                {
+                    Token = table.Column<string>(type: "text", nullable: false),
+                    UserId = table.Column<Guid>(type: "uuid", nullable: true),
+                    ExpireDate = table.Column<DateTime>(type: "timestamp without time zone", nullable: false),
+                    UserAgent = table.Column<string>(type: "text", nullable: true),
+                    IPAddress = table.Column<string>(type: "text", nullable: true),
+                    IsRemembered = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AuthTokens", x => x.Token);
+                    table.ForeignKey(
+                        name: "FK_AuthTokens_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Emails",
+                columns: table => new
+                {
+                    EmailAddress = table.Column<string>(type: "text", nullable: false),
+                    UserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    IsVerified = table.Column<bool>(type: "boolean", nullable: false),
+                    VerificationToken_Token = table.Column<string>(type: "text", nullable: true),
+                    VerificationToken_CreatedAt = table.Column<DateTime>(type: "timestamp without time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Emails", x => x.EmailAddress);
+                    table.ForeignKey(
+                        name: "FK_Emails_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PasswordCredentials",
+                columns: table => new
+                {
+                    HashedPassword = table.Column<string>(type: "text", nullable: false),
+                    UserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    PasswordSalt = table.Column<string>(type: "text", nullable: true),
+                    PasswordResetToken_Token = table.Column<string>(type: "text", nullable: true),
+                    PasswordResetToken_CreatedAt = table.Column<DateTime>(type: "timestamp without time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PasswordCredentials", x => x.HashedPassword);
+                    table.ForeignKey(
+                        name: "FK_PasswordCredentials_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserPrivilege",
+                columns: table => new
+                {
+                    UserId = table.Column<Guid>(type: "uuid", nullable: false),
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    PrivilegeName = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserPrivilege", x => new { x.UserId, x.Id });
+                    table.ForeignKey(
+                        name: "FK_UserPrivilege_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ArticleStep",
+                columns: table => new
+                {
+                    ArticlePartArticleId = table.Column<Guid>(type: "uuid", nullable: false),
+                    ArticlePartId = table.Column<int>(type: "integer", nullable: false),
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    StepNumber = table.Column<int>(type: "integer", nullable: false),
+                    Title = table.Column<string>(type: "text", nullable: true),
+                    Description = table.Column<string>(type: "text", nullable: true),
+                    ImageId = table.Column<Guid>(type: "uuid", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ArticleStep", x => new { x.ArticlePartArticleId, x.ArticlePartId, x.Id });
+                    table.ForeignKey(
+                        name: "FK_ArticleStep_ArticlePart_ArticlePartArticleId_ArticlePartId",
+                        columns: x => new { x.ArticlePartArticleId, x.ArticlePartId },
+                        principalTable: "ArticlePart",
+                        principalColumns: new[] { "ArticleId", "Id" },
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ArticleStep_Image_ImageId",
+                        column: x => x.ImageId,
+                        principalTable: "Image",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Articles_HeroImageId",
+                table: "Articles",
+                column: "HeroImageId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ArticleStep_ImageId",
+                table: "ArticleStep",
+                column: "ImageId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ArticleUser_UsersId",
+                table: "ArticleUser",
+                column: "UsersId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AuthTokens_UserId",
+                table: "AuthTokens",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Emails_UserId",
+                table: "Emails",
+                column: "UserId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_PasswordCredentials_UserId",
+                table: "PasswordCredentials",
+                column: "UserId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_ProfilePictureId",
+                table: "Users",
+                column: "ProfilePictureId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ArticleStep");
+
+            migrationBuilder.DropTable(
+                name: "ArticleUser");
+
+            migrationBuilder.DropTable(
+                name: "AuthTokens");
+
+            migrationBuilder.DropTable(
+                name: "Emails");
+
+            migrationBuilder.DropTable(
+                name: "PasswordCredentials");
+
+            migrationBuilder.DropTable(
+                name: "UserPrivilege");
+
+            migrationBuilder.DropTable(
+                name: "ArticlePart");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropTable(
+                name: "Articles");
+
+            migrationBuilder.DropTable(
+                name: "Image");
+        }
+    }
+}
